d6ef3b0a78329219ebac10dc499589e3381c638d
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-08-06 01:40:18 +0530

    "12 Mega Project part 1: Done all the configuration of appwrite connections, environment variables and ENV confi."

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { conf } from "./conf/conf.js";
+
+function App() {
+  console.log(conf.appWriteUrl);
+  return (
+    <>
+      <h1>Mega Project</h1>
+    </>
+  );
+}
+
+export default App;

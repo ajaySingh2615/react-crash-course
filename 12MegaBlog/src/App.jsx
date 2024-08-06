a2995b13b25af5bd7b921b114d4ee5533f23c9ac
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-08-06 14:05:42 +0530

    "12 Mega Project part 4: how to configure redux toolkit."

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,13 +1,40 @@
 import "./App.css";
-import { conf } from "./conf/conf.js";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth.js";
+import { login, logout } from "./store/authSlice.js";
+import { Footer, Header } from "./components/index.js";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  console.log(conf.appWriteUrl);
-  return (
-    <>
-      <h1>Mega Project</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          console.log(",log in");
+          dispatch(login({ userData }));
+        } else {
+          console.log("Log out");
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-500">
+      <div className="w-full block">
+        <Header />
+        <main> TODO:{/*<Outlet />*/}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
